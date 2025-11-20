10d69c3feaf755ae16a104c7ebfacf86d1cdba13
AtharvBolke99 <atharvbolke99@gmail.com>
2025-11-20 22:54:06 +0530

    form handling concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dataForm, setDataForm] = useState({
+    name: "",
+    email: "",
+    error: "",
+  });
 
+  useEffect(() => {
+    if ( dataForm.name.length >0 && dataForm.name.length < 3) {
+      setDataForm({
+        ...dataForm,
+        error: "name must be greater than 3 character",
+      });
+    } else if (dataForm.name > 20) {
+      setDataForm({
+        ...dataForm,
+        error: "name must be less than 20 character",
+      });
+    } else {
+      setDataForm({ ...dataForm, error: "" });
+    }
+  }, [dataForm.name, dataForm.email]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem(dataForm));
+    if (storedData) {
+      setDataForm({
+        ...dataForm,
+        name: storedData.name,
+        email: storedData.email,
+      });
+    }
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <h1 className="header">Form Handling</h1>
+      <p>{dataForm.name}</p>
+      <p>{dataForm.email}</p>
+      <p>{dataForm.error}</p>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          className="input-name"
+          value={dataForm.name}
+          onChange={(e) => {
+            setDataForm({ ...dataForm, name: e.target.value });
+          }}
+        />
+        <input
+          type="email"
+          placeholder="Enter Your mail"
+          className="input-name"
+          value={dataForm.email}
+          onChange={(e) => {
+            setDataForm({ ...dataForm, email: e.target.value });
+          }}
+        />
+        <button
+          className="btn"
+          onClick={() => {
+            if (!dataForm.name) {
+              alert("error");
+              return;
+            }
+            localStorage.setItem("dataForm", JSON.stringify(dataForm));
+            setDataForm({ name: "", email: "", error: "" });
+          }}
+        >
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
